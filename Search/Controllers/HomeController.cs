3683f6f819f49aca6ad0ea395d98461f8e7e2f36
Firuze <pfiruza.p202@code.edu.az>
2017-12-23 16:22:20 +0400

    search

---
+++ Search/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Search.Models;
+
+namespace Search.Controllers
+{
+    public class HomeController : Controller
+    {
+        SearchEntities db = new SearchEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.info.ToList());
+        }
+
+        public JsonResult GetSearchData(string searchBy, string searchVal)
+        {
+            List<info> Lists = new List<info>();
+
+            if (searchBy == "id")
+            {
+                try
+                {
+                    int Id = Convert.ToInt32(searchVal);
+                    Lists = db.info.Where(a => a.id == Id || searchVal == null).ToList();
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("{0} is not a Id", searchVal);
+                }
+                return Json(Lists, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                Lists = db.info.Where(a => a.name.Contains(searchVal) || searchVal == null).ToList();
+                return Json(Lists, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ Search/Scripts/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        var searchBy = $("#searchBy").val();
+        //console.log(searchBy)
+        var searchVal = $("#Search").val();
+        //console.log(searchVal)
+        var setData = $("#dataSearch");
+        //console.log(setData)
+        setData.html("");
+        $.ajax({
+            type: "post",
+            url: "/Home/GetSearchData?searchBy=" + searchBy + "&searchVal=" + searchVal,
+            contentType: "html",
+            success: function (result) {
+                if (result.length == 0) {
+                    setData.append('<tr style="color:red"><td colspan="3">No match data</td></tr>')
+                }
+                else {
+                    $.each(result, function (index, value) {
+                        var Data = "<tr>" + "<td>" + value.id + "</td>" + "<td>" + value.name + "</td>" + "<td>" + value.degree + "</td>" + "</tr>";
+                        setData.append(Data);
+                    });
+                }
+            }
+
+        });
+    });
+
+});
